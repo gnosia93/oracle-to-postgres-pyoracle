28f697b3292e58ab124f2948fe0768c3d52c5dbf
Kwon <soonbeom@f8ffc2077dc2.ant.amazon.com>
2021-01-19 18:37:57 +0900

    ..

--- create-schema.sh
+++ create-schema.sh
@@ -1,13 +1,20 @@
 #! /bin/bash
-grep $ORACLE_HOME/tnsnames.ora -e '<your-oracle-private-ip>'
-if [ $? == 0 ]; then
-  echo "finding oracle ec2 private ip ...."
-  PRIVATE_IP_ADDR=`aws ec2 describe-instances --region=ap-northeast-2 \
-  --filters "Name=tag:Name,Values=tf_oracle_11xe" \
-  --query "Reservations[*].Instances[*].{PrivateIpAddress:PrivateIpAddress}" \
-  --output=text`
 
-  sed -i "s/<your-oracle-private-ip>/$PRIVATE_IP_ADDR/g" $ORACLE_HOME/tnsnames.ora
-fi
+function find_raplce_oracle_ip() {
+  #grep $ORACLE_HOME/tnsnames.ora -e '<your-oracle-private-ip>'
+  grep $1 -e '<your-oracle-private-ip>'
+  if [ $? == 0 ]; then
+    echo "finding oracle ec2 private ip ...."
+    PRIVATE_IP_ADDR=`aws ec2 describe-instances --region=ap-northeast-2 \
+    --filters "Name=tag:Name,Values=tf_oracle_11xe" \
+    --query "Reservations[*].Instances[*].{PrivateIpAddress:PrivateIpAddress}" \
+    --output=text`
+
+    sed -i "s/<your-oracle-private-ip>/$PRIVATE_IP_ADDR/g" $ORACLE_HOME/tnsnames.ora
+  fi
+}
+
+find_raplce_oracle_ip $ORACLE_HOME/tnsnames.ora
+find_raplce_oracle_ip /home/ec2-user/pyoracle/config.ini
 
 sqlplus system/manager@xe @create-schema.sql
