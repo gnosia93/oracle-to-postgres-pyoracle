baa9a9409c36d64354ce37185ac819bc07f21949
Kwon <soonbeom@f8ffc2077dc2.ant.amazon.com>
2021-01-19 18:16:15 +0900

    ..

--- create-schema.sh
+++ create-schema.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
-PRIVATE_IP_ADDR=`aws ec2 describe-instances --region=ap-northeast-2 \
---filters "Name=tag:Name,Values=tf_oracle_11xe" \
---query "Reservations[*].Instances[*].{PrivateIpAddress:PrivateIpAddress}" \
---output=text`
+grep $ORACLE_HOME/tnsnames.ora -e '<your-oracle-private-ip>'
+if [ $? == 0 ]; then
+  echo "finding oracle ec2 private ip ...."
+  PRIVATE_IP_ADDR=`aws ec2 describe-instances --region=ap-northeast-2 \
+  --filters "Name=tag:Name,Values=tf_oracle_11xe" \
+  --query "Reservations[*].Instances[*].{PrivateIpAddress:PrivateIpAddress}" \
+  --output=text`
 
-sed -i "s/<your-oracle-private-ip>/$PRIVATE_IP_ADDR/g" $ORACLE_HOME/tnsnames.ora
+  sed -i "s/<your-oracle-private-ip>/$PRIVATE_IP_ADDR/g" $ORACLE_HOME/tnsnames.ora
+fi
+
 sqlplus system/manager@xe @create-schema.sql
