f445b7011ecc221ea4c6bba398f965c2a02aa278
Kwon <soonbeom@f8ffc2077dc2.ant.amazon.com>
2021-01-19 18:08:42 +0900

    ..

--- create-schema.sh
+++ create-schema.sh
@@ -1,3 +1,8 @@
-aws ec2 describe-instances --region=ap-northeast-2 --filters "Name=Instance,Values=tf_oracle_11xe"
+#! /bin/bash
+PRIVATE_IP_ADDR=`aws ec2 describe-instances --region=ap-northeast-2 \
+--filters "Name=tag:Name,Values=tf_oracle_11xe" \
+--query "Reservations[*].Instances[*].{PrivateIpAddress:PrivateIpAddress}" \
+--output=text`
 
-sqlplus system/manager @create-schema.sql
+sed -i "s/<your-oracle-private-ip>/$PRIVATE_IP_ADDR/g" $ORACLE_HOME/tnsnames.ora
+sqlplus system/manager@xe @create-schema.sql
