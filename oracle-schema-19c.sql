8bce7a17bb0da84fdc148e9d67fab39fbdc32454
Kwon <soonbeom@f8ffc2077dc2.ant.amazon.com>
2021-02-02 09:07:07 +0900

    ..

--- oracle-schema-19c.sql
+++ oracle-schema-19c.sql
@@ -113,7 +113,8 @@ create table shop.tb_order_detail
 
 
 -- added 2021/02/01
-create or replace view view_recent_order_30 as
+-- view
+create or replace view shop.view_recent_order_30 as
 select name, order_no, member_id, order_price, order_ymdt
 from (
     select rownum as rn, p.name, o.order_no, o.member_id, o.order_price, o.order_ymdt
@@ -123,5 +124,77 @@ from (
     order by o.order_ymdt desc
 )
 where rn between 1 and 30;
+
+-- function
+DROP FUNCTION shop.get_product_id;
+CREATE OR REPLACE FUNCTION shop.get_product_id
+RETURN VARCHAR2
+IS
+    v_today           VARCHAR2(8);
+    v_sub_order_no    VARCHAR(12);
+BEGIN
+
+    select to_char(sysdate, 'yyyymmdd') into v_today from dual;
+    select lpad(shop.seq_order_order_id.nextval, 12, '0') into v_sub_order_no from dual;
+
+    return v_today || v_sub_order_no;
+END;
+/
+
+-- procedure
+drop table shop.tb_product_summary;
+create table shop.tb_product_summary
+(
+   year           varchar2(4) not null,
+   month          varchar2(2) not null,
+   day            varchar2(2) not null,
+   hour           varchar2(2) not null,
+   min            varchar2(2) not null,
+   product_id     number(9) not null,
+   order_cnt      number not null,
+   total_price    number not null,
+   comment_cnt    number not null,
+   primary key(year, month, day, hour, min, product_id)
+);
+
+CREATE OR REPLACE PROCEDURE shop.sp_product_summary(v_interval in number)
+IS
+    v_cnt NUMBER := 0;
+BEGIN
+    -- truncate table.
+    execute immediate 'truncate table test_table';
+
+    insert into shop.tb_product_summary
+    select to_char(o.order_ymdt, 'yyyy') as year,
+        to_char(o.order_ymdt, 'mm') as month,
+        to_char(o.order_ymdt, 'dd') as day,
+        to_char(o.order_ymdt, 'hh') as hour,
+        to_char(o.order_ymdt, 'mm') as min,
+        p.product_id,
+        count(1) as order_cnt,
+        sum(d.product_price) as total_price,
+        max(c.comment_cnt) as comment_cnt
+    from
+        shop.tb_order o,
+        shop.tb_order_detail d,
+        shop.tb_product p,
+        (select product_id, count(1) as comment_cnt
+         from shop.tb_comment
+         group by product_id) c
+    where o.order_no = d.order_no
+      and d.product_id = p.product_id
+      and p.product_id = c.product_id(+)
+    group by to_char(o.order_ymdt, 'yyyy'),
+        to_char(o.order_ymdt, 'mm'),
+        to_char(o.order_ymdt, 'dd'),
+        to_char(o.order_ymdt, 'hh'),
+        to_char(o.order_ymdt, 'mm'),
+        p.product_id;
+
+    COMMIT;
+END;
+/
+
+
 
 quit
